421138d6537da3c1ce399c27fec6d69953c8897e
Abraham Ruiz <abraham@Mac-mini.local>
2024-01-09 19:13:57 -0600

    add beaKey functions

---
+++ R/beaKey.R
@@ -0,0 +1,17 @@
+beaSetKey <- function(APIkey = ""){
+  if (!rlang::is_string(APIkey))
+    rlang::abort("'APIkey` must be a string.")
+  if (nchar(APIkey)!=36){
+    warning(paste0("'Invalid API Key: ",APIkey,". Register @ <https://apps.bea.gov/API/signup/>'"))
+    return(paste0("'Invalid API Key: ",APIkey,". Register @ <https://apps.bea.gov/API/signup/>'"))
+  }
+  Sys.setenv(beaKey = APIkey)
+}
+
+beaGetKey <- function(){
+  key <- Sys.getenv(beaKey)
+  if (is.na(key)){
+    return(NULL)
+    }
+  return(key)
+}
