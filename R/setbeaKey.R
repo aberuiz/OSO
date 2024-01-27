c5c2abd1aa025cde16b22ea5718cfb4fe0a9faaf
Abraham Ruiz <abraham@Mac-mini.local>
2024-01-27 15:00:45 -0600

    key and desc updates

--- R/setbeaKey.R
+++ R/setbeaKey.R
@@ -1,17 +1,17 @@
-setbeaKey <- function(APIkey, overwrite = FALSE, install = FALSE){
+setbeaKey <- function(APIkey, install = FALSE, overwrite = FALSE){
 
   if (!rlang::is_string(APIkey)){
-    rlang::abort("'APIkey` must be a string.")
+    stop("'APIkey` must be a string.")
   }
   if (nchar(APIkey)!=36){
-    warning(paste0("'Invalid API Key: ",APIkey," Register @ <https://apps.bea.gov/API/signup/>'"))
+    stop(paste0("'Invalid API Key: ",APIkey," Register @ <https://apps.bea.gov/API/signup/>'"))
   }
 
   if (install) {
     home <- Sys.getenv("HOME")
     renv <- file.path(home, ".Renviron")
     if(file.exists(renv)){
-      # Backup original .Renviron before doing anything else here.
+      # Backup original .Renviron
       file.copy(renv, file.path(home, ".Renviron_backup"))
     }
     if(!file.exists(renv)){
@@ -20,7 +20,7 @@ setbeaKey <- function(APIkey, overwrite = FALSE, install = FALSE){
     else{
       if(isTRUE(overwrite)){
         message("Your original .Renviron will be backed up in R HOME directory.")
-        oldenv=read.table(renv, stringsAsFactors = FALSE)
+        oldenv = read.table(renv, stringsAsFactors = FALSE)
         newenv <- oldenv[-grep("beaKey", oldenv),]
         write.table(newenv, renv, quote = FALSE, sep = "\n",
                     col.names = FALSE, row.names = FALSE)
@@ -33,9 +33,9 @@ setbeaKey <- function(APIkey, overwrite = FALSE, install = FALSE){
       }
     }
 
-    keyconcat <- paste0("beaKey='", APIkey, "'")
+    newKey <- paste0("beaKey='", APIkey, "'")
     # Append API key to .Renviron file
-    write(keyconcat, renv, sep = "\n", append = TRUE)
+    write(newKey, renv, sep = "\n", append = TRUE)
     message('Your BEA API key has been stored in your .Renviron and can be accessed by Sys.getenv("beaKey"). \nTo use now, restart R or run `readRenviron("~/.Renviron")`')
     return(beaKey)
   } else {
