14f74fadb6afe231254980f8a39debf5c0f9d577
Abraham Ruiz <abraham@Mac-mini-2.local>
2024-03-31 11:43:23 -0500

    MNE improvements: GetFootnotes must be "yes"

--- R/beaMNE.R
+++ R/beaMNE.R
@@ -1,4 +1,4 @@
-beaMNE <- function(SeriesID = "", DirectionOfInvestment = "", Year = "", Classification = "", Industry = "", Country = "", State = "", GetFootnotes = "", ResultFormat = "json", beaKey = NULL){
+beaMNE <- function(SeriesID = "", DirectionOfInvestment = "", Year = "", Classification = "", Industry = "", NonBankAffiliates = NULL, OwnershipLevel = NULL, Country = "", State = "", GetFootnotes = "yes", ResultFormat = "json", beaKey = NULL){
   if (is.null(beaKey)){
     beaKey <- getbeaKey()
   }
@@ -6,6 +6,10 @@ beaMNE <- function(SeriesID = "", DirectionOfInvestment = "", Year = "", Classif
     warning(paste0("Invalid API Key: ",beaKey," Register <https://apps.bea.gov/API/signup/> Store with `setbeaKey`"))
     return(paste0("Invalid API Key: ",beaKey," Register <https://apps.bea.gov/API/signup/> Store with `setbeaKey`"))
   }
+  # Direct Investment
+#  if (is.null(OwnershipLevel))
+
+  # Activities of MNEs
   response <- httr2::request("https://apps.bea.gov/api/data") |>
     httr2::req_url_query(
       'UserID' = beaKey,
@@ -16,6 +20,8 @@ beaMNE <- function(SeriesID = "", DirectionOfInvestment = "", Year = "", Classif
       'Year' = Year,
       'Classification' = Classification,
       'Industry' = Industry,
+      'NonBankAffiliatesOnly' = NonBankAffiliates,
+      'OwnershipLevel' = OwnershipLevel,
       'Country' = Country,
       'State' = State,
       'GetFootnotes' = GetFootnotes,
@@ -23,6 +29,21 @@ beaMNE <- function(SeriesID = "", DirectionOfInvestment = "", Year = "", Classif
     ) |>
     httr2::req_perform() |>
     httr2::resp_body_json()
+   if ("Error" %in% names(response$BEAAPI)) {
+     warning(paste0(response$BEAAPI$Error$APIErrorCode,": ",response$BEAAPI$Error$APIErrorDescription))
+     return(dplyr::bind_rows(response$BEAAPI$Error))
+   }
+ data <- dplyr::bind_rows(response$BEAAPI$Results$Data)
+# data$DataValue <- as.numeric(data$DataValue)
+ # names(data)[7] <- gsub(" ","_",
+ #                        gsub(":","",response$BEAAPI$Results$Statistic))
+ if (GetFootnotes == "yes"){
+   print(dplyr::bind_rows(response$BEAAPI$Results$Notes))
+ } else break
+ # notes <- dplyr::bind_rows(response$BEAAPI$Results$Notes)
+ # message(response$BEAAPI$Results$Statistic)
+ # print(paste(notes[[2]]))
+ return(data)
 }
 
 ## Need to Add
